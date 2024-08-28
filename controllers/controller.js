9690f40ccd82fa0bbec1cadc2682d542d4b989e2
iadam2000 <ibby200@hotmail.co.uk>
2024-08-28 11:45:51 +0100

    Ammended task 2 to include error handling

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,8 @@
 const { fetchTopics } = require('../models/models');
 
-exports.getTopics = ( req, res, ) => {
+exports.getTopics = (req, res, next) => {
     fetchTopics().then(data => {
-        return res.status(200).send({ topics: data.rows });
-    });
+        return res.status(200).send({ topics: data });
+    })
+    .catch(next);
 };
